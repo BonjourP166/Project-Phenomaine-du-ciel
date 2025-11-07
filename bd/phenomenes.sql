4fc78326b8297c775198ecd206586ce6c6297418
Ioanna Kortara <joannek.7171@gmail.com>
2025-11-07 10:04:18 +0100

    Add files via upload

---
+++ bd/phenomenes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE phenomenes(id_phénomene INT PRIMARY KEY,nom VARCHAR(10),description TEXT);
+INSERT INTO phenomenes (id_phénomene,nom,description) VALUES 
+(1,'metéorite','Fragment de roche ou de métal provenant de l’espace qui atteint la surface de la Terre.'),
+(2,'bolide','Météore très lumineux, souvent accompagné d’un fort sifflement ou d’un bruit de tonnerre, généralement plus grand qu’un météore ordinaire.'),
+(3,'éclipse lunaire','Phénomène où la Lune entre dans l’ombre de la Terre, pouvant être totale, partielle ou pénombrale.'),
+(4,'éclipse solaire','Phénomène où la Lune passe devant le Soleil, obscurcissant partiellement ou totalement sa lumière depuis la Terre.');
