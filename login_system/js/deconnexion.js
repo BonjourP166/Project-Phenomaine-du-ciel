e37039c297ac53d03480cbd02e808162b163d660
SantiRoques <santiagoroque2020@gmail.com>
2025-11-16 21:21:18 +0100

    Update deconnexion.js

--- login_system/js/deconnexion.js
+++ login_system/js/deconnexion.js
@@ -1,1 +1,29 @@
+// login_system/js/deconnexion.js
+
+$(document).ready(function () {
+
+  $("#btn_deconnexion").click(function (event) {
+    event.preventDefault();
+
+    $.ajax({
+      url: "deconnexion.php",
+      method: "GET",
+      dataType: "json",
+
+      success: function (reponse) {
+        if (reponse.success && reponse.redirect) {
+          window.location.href = reponse.redirect;
+        } else {
+          window.location.reload();
+        }
+      },
+
+      error: function (xhr, status, error) {
+        console.error("Erreur Ajax déconnexion :", status, error);
+        alert("Erreur lors de la déconnexion.");
+      }
+    });
+  });
+
+});
 
