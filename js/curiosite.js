67739c2af39241f6961b716194522279e77fc25b
SantiRoques <santiagoroque2020@gmail.com>
2025-11-16 21:23:46 +0100

    Update curiosite.js

--- js/curiosite.js
+++ js/curiosite.js
@@ -1,1 +1,25 @@
+// js/curiosite.js
+
+$(document).ready(function () {
+
+  $("#btn_random_curiosite").click(function () {
+
+    $.ajax({
+      url: "curiosite.php",
+      method: "GET",
+      data: { ajax: 1, action: "random" },
+      dataType: "html",
+
+      success: function (html) {
+        $("#zone_curiosite").html(html);
+      },
+
+      error: function (xhr, status, error) {
+        console.error("Erreur Ajax curiosite :", status, error);
+      }
+    });
+
+  });
+
+});
 
