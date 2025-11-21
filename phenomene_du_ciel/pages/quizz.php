8659396d1aa25363ddb1ad4dcdcb198930d1b3c5
Margaux Bresson <bonjourpoubelle166@gmail.com>
2025-11-21 09:45:41 +0100

    Add files via upload

---
+++ phenomene_du_ciel/pages/quizz.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Univers en Direct</title>
+  <link rel="stylesheet" href="../styles/css_Principale.css">
+  <link rel="stylesheet" href="../styles/css_Banieres.css">
+</head>
+<body>
+
+  <?php include 'includes/header.php'; ?>
+
+
+  <div class="quizz">
+
+  <h2> Testez vos connaissances sur les phénomènes du ciel !</h2>
+  
+  <p>Êtes-vous un simple observateur du ciel…ou un véritable explorateur des étoiles ?</p>
+  <p>Ce quiz vous plongera au cœur des éclipses, météorites, bolides et autres merveilles célestes.
+    Répondez à quelques questions amusantes pour tester votre culture astronomique !</p>
+  <p>Durée estimée : 2 minutes — 6 questions</p>
+  </div>
+
+    <div class="quizz_bouton">
+  <p>Commencer le Quizz</p>
+  </div>
+
+ 
+  <?php include 'includes/footer.php'; ?>
+</body>
+</html>
