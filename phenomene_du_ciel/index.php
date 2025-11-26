fdbf195e745a3dd810c6a8fdd500704bf62bbbad
SantiRoques <santiagoroque2020@gmail.com>
2025-11-26 14:52:38 +0100

    Update index.php

--- phenomene_du_ciel/index.php
+++ phenomene_du_ciel/index.php
@@ -1,48 +1,37 @@
-<header class="site-header">
-  <div class="logo">
-    <img src="images/logo.jpg" alt="Logo" class="cercle1"> 
-    <a href="index.php" class="a_h2">Univers en direct</a>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Univers en Direct</title>
+  <link rel="stylesheet" href="styles/css_page_acceuil.css">
+  <link rel="stylesheet" href="styles/css_banieres.css">
+  <link rel="icon" type="image/jpg" href="images/logo.jpg">
+</head>
+<body>
+  <?php include 'includes/header.php'; ?>
+
+  <main class="hero">
+    <h1>Phénomène du Ciel</h1>
+    <p>Découvrez les phénomènes célestes : météorites, bolides, éclipses solaires et lunaires.</p>
+  
+
+  <div class="images-droite">
+  <a href="pages/bolides.php"><img src="images/bolides.jpg"></a>
+  <a href="pages/eclipses_lunaires.php"><img src="images/eclipses_lunaires.jpg"></a>
+  <a href="pages/eclipses_solaires.php"><img src="images/eclipses_solaires.jpg"></a>
+  <a href="pages/meteorites.php"><img src="images/meteorites.jpg"></a>
   </div>
 
-  <nav class="header">
-    <!-- Menu déroulant Phénomène -->
-    <div class="dropdown">
-      <a href="javascript:void(0);" class="dropbtn">Phénomène ▼</a>
-      <div class="dropdown-content">
-        <a href="pages/phenomene.php?type=1" class="no-underline">Météorites</a>
-        <a href="pages/phenomene.php?type=2" class="no-underline">Bolide</a>
-        <a href="pages/phenomene.php?type=3" class="no-underline">Éclipses Solaires</a>
-        <a href="pages/phenomene.php?type=4" class="no-underline">Éclipses Lunaires</a>
-      </div>
-    </div>
 
-    <a href="pages/carte.php">Carte</a>
-    <a href="pages/frise.php">Frise</a>
-    <a href="pages/forum.php">Forum</a>
-    <a href="pages/curiosite.php">Curiosité</a>
-    <a href="pages/quizz.php">Quizz</a>
-    <a href="login_system/formulaire_connextions.php">Connexion</a>
-    <a href="pages/profile.php">
-      <img src="images/profile.png" alt="Profil" class="cercle3">
-    </a>
-  </nav>
-</header>
+    <h2>Grâce à nous, vous pouvez observer et comprendre 
+les phénomènes offerts par le ciel.</h2>
+    
+  </main>
 
+  <?php include 'includes/footer.php'; ?>
+</body>
 
-<script>
-const dropbtn = document.querySelector('.dropbtn');
-const dropdownMenu = document.querySelector('.dropdown-content');
-
-// Ouvrir/fermer le menu au clic
-dropbtn.addEventListener('click', (e) => {
-  e.stopPropagation(); // empêche de déclencher le clic sur window
-  dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
-});
-
-// Fermer si on clique en dehors
-window.addEventListener('click', () => {
-  dropdownMenu.style.display = 'none';
-});
-</script>
+</html>
+----------
 
 
