62ad23ee711f6339600637ccea76f6e565462a7c
Margaux Bresson <bonjourpoubelle166@gmail.com>
2025-11-27 02:16:16 +0100

    Add files via upload

---
+++ phenomene_du_ciel/pages/box.php
@@ -0,0 +1,17 @@
+<?php
+echo "<h3>Ecrivé votre commentaire !</h3>";
+echo '<form id="formConnexion" method="POST">
+
+    <p>-Titre :
+        <input type="teste" name="titre" >
+    </p>
+
+    <p>-Contenue :
+        <input type="texte" name="contenue">
+    </p>
+
+    <p>
+        <input type="submit" value="publier">
+    </p>
+</form>';
+?>
---
+++ phenomene_du_ciel/pages/profile.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Modifier Profil</title>
+  <link rel="stylesheet" href="../styles/css_principale.css">
+  <link rel="stylesheet" href="../styles/css_container.css">
+  <link rel="stylesheet" href="../styles/css_banieres.css">
+  <link rel="icon" type="image/jpg" href="../images/logo.jpg">
+   
+</head>
+<body>
+
+<?php include 'includes/header.php'; ?>
+<div class="form-container">
+    <h2>Modifier votre profil</h2>
+
+</div>
+
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
